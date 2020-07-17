fcb181c0469061288a591e2c33ba4956578106e9
sarika296 <sarikadewangan94@gmail.com>
2020-07-17 11:00:35 +0530

    added footer, welcome, navabar and route code

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import NavigationBar from './components/NavigationBar';
+import Welcome from './components/Welcome';
+import Book from './components/Book';
+import BookList from './components/BookList';
+import Footer from './components/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Welcome to Book Shop
-      </p>
-    </div>
-  );
-}
+    const marginTop = {
+        marginTop:"20px"
+    };
 
-export default App;
+    return (
+        <Router>
+            <NavigationBar/>
+            <Container>
+                <Row>
+                    <Col lg={12} style={marginTop}>
+                        <Switch>
+                            <Route path="/" exact component={Welcome}/>
+                            <Route path="/add" exact component={Book}/>
+                            <Route path="/list" exact component={BookList}/>
+                        </Switch>
+                    </Col>
+                </Row>
+            </Container>
+            <Footer/>
+        </Router>
+    );
+}
+export default App
---
+++ src/main/reactjs/src/components/BookList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class BookList extends React.Component {
+    render() {
+        return (<div className="text-white">Book List</div>);
+    }
+}
