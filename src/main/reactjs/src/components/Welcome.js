b53ba51b38153aaf88f7d67c2c1758265afd10a2
sarika296 <sarikadewangan94@gmail.com>
2020-07-17 12:19:21 +0530

    added fuction related changes for welcome again

--- src/main/reactjs/src/components/Welcome.js
+++ src/main/reactjs/src/components/Welcome.js
@@ -2,22 +2,18 @@ import React from 'react';
 
 import {Jumbotron} from 'react-bootstrap';
 
-class Welcome extends React.Component {
-    render() {
-        return (
-            <Jumbotron className="bg-dark text-white">
-                <h1>Welcome to Book Shop</h1>
-                <blockquote className="blockquote mb-0">
-                    <p>
-                        Good friends, good books, and a sleepy conscience: this is the ideal life.
-                    </p>
-                    <footer className="blockquote-footer">
-                        Mark Twain
-                    </footer>
-                </blockquote>
-            </Jumbotron>
-        );
-    }
+export default function Welcome(props) {
+    return (
+        <Jumbotron className="bg-dark text-white">
+            <h1>{props.heading}</h1>
+            <blockquote className="blockquote mb-0">
+                <p>
+                    {props.quote}
+                </p>
+                <footer className="blockquote-footer">
+                    {props.footer}
+                </footer>
+            </blockquote>
+        </Jumbotron>
+    );
 }
-
-export default Welcome;
