7f261d0496b987f398e80c2fabc7fe5645936ee4
sarika296 <sarikadewangan94@gmail.com>
2020-07-17 10:44:56 +0530

    initial import

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        Welcome to Book Shop
+      </p>
+    </div>
+  );
+}
+
+export default App;
