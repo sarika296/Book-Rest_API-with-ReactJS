24237dedcd97220d8e2a6a820e46a53ea6432793
sarika296 <sarikadewangan94@gmail.com>
2020-07-17 12:17:30 +0530

    added function related changes for welcome page

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-
 import NavigationBar from './components/NavigationBar';
 import Welcome from './components/Welcome';
 import Book from './components/Book';
@@ -9,10 +8,14 @@ import BookList from './components/BookList';
 import UserList from './components/UserList';
 import Footer from './components/Footer';
 
-function App() {
+export default function App() {
     const marginTop = {
         marginTop:"20px"
     };
+
+    const heading = "Welcome to Book Shop";
+    const quote = "Good friends, good books, and a sleepy conscience: this is the ideal life.";
+    const footer = "Mark Twain";
 
     return (
         <Router>
@@ -21,7 +24,7 @@ function App() {
                 <Row>
                     <Col lg={12} style={marginTop}>
                         <Switch>
-                            <Route path="/" exact component={Welcome}/>
+                            <Route path="/" exact component={() => <Welcome heading={heading} quote={quote} footer={footer}/>}/>
                             <Route path="/add" exact component={Book}/>
                             <Route path="/edit/:id" exact component={Book}/>
                             <Route path="/list" exact component={BookList}/>
@@ -34,5 +37,3 @@ function App() {
         </Router>
     );
 }
-
-export default App;
